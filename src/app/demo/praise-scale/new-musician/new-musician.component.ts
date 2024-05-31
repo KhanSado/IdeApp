f427ad071fc1372c89400e2f18ce80e9c13e15f2
Berson <bersonintech@gmail.com>
2024-05-31 19:16:09 -0300

    cadastros iniciais de musico e instrumentos

--- src/app/demo/praise-scale/new-musician/new-musician.component.ts
+++ src/app/demo/praise-scale/new-musician/new-musician.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
+import { CultControlService } from '../../cult-control/service/cult-control.service';
+import { PraiseScaleService } from '../praise-scale.service';
 
 @Component({
   selector: 'app-new-musician',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule, RouterModule],
   templateUrl: './new-musician.component.html',
   styleUrl: './new-musician.component.scss'
 })
-export class NewMusicianComponent {
+export class NewMusicianComponent implements OnInit{
 
+  newMusicianForm!: FormGroup
+  instrumentList: Instrument[] = [];
+
+  constructor(private service: PraiseScaleService) { }
+
+  ngOnInit(): void {
+    this.findInstruments()
+    this.newMusicianForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      restrictions: new FormControl(''),
+      instrument: new FormControl('', [Validators.required])
+    })
+  }
+  get name() {
+    return this.newMusicianForm.get('name')!
+  }
+  get restrictions() {
+    return this.newMusicianForm.get('restrictions')!
+  }
+  get instrument() {
+    return this.newMusicianForm.get('instrument')!
+  }
+
+  registerMusician(){
+    this.service.createMusician({
+      id: "",
+      name: this.name.value,
+      restrictions: this.restrictions.value,
+      instrument: this.instrument.value
+    }).then(() => {
+      // Limpar os campos após salvar
+      this.name.reset();
+      this.restrictions.reset();
+      this.instrument.reset();
+    }).catch((error) => {
+      console.error('Erro ao criar musico: ', error);
+    });
+  }
+
+  async findInstruments() {
+    try {
+      const documents = await this.service.findInstruments();
+      if (documents && documents.length > 0) {
+        this.instrumentList = documents;
+      } else {
+        console.log('Nenhum documento encontrado');
+      }
+    } catch (error) {
+      console.error('Erro ao buscar documentos:', error);
+    }
+  }
+}
+
+type Instrument = {
+  id: string;
+  instrumentName: string;
 }
--- src/app/demo/praise-scale/praise-scale.service.ts
+++ src/app/demo/praise-scale/praise-scale.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, DocumentData } from '@angular/fire/compat/firestore';
+import firebase from 'firebase/compat/app';
 import Swal from 'sweetalert2';
 
 @Injectable({
@@ -12,49 +13,131 @@ export class PraiseScaleService implements OnInit {
   ngOnInit(): void {
   }
 
-createInstrument(params: Instrument): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    if (params.instrumentName.length > 0) {
-      this.firestore.collection('instrument').add({
-        name: params.instrumentName,
-      }).then(instrumentRef => {
-        const instrumentId = instrumentRef.id;
-        return this.firestore.doc(`instrument/${instrumentId}`).update({
-          id: instrumentId
-        }).then(() => {
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Novo instrumento adicionado",
-            showConfirmButton: true,
-            confirmButtonText: "Ok",
-            timer: 1500
+  createInstrument(params: Instrument): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (params.instrumentName.length > 0) {
+        this.firestore.collection('instrument').add({
+          instrumentName: params.instrumentName,
+        }).then(instrumentRef => {
+          const instrumentId = instrumentRef.id;
+          return this.firestore.doc(`instrument/${instrumentId}`).update({
+            id: instrumentId
+          }).then(() => {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Novo instrumento adicionado",
+              showConfirmButton: true,
+              confirmButtonText: "Ok",
+              timer: 1500
+            });
+            resolve();
           });
-          resolve();
+        }).catch(error => {
+          console.error('Error creating reception team:', error);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Ocorreu um erro ao cadastrar novo instrumento"
+          });
+          reject(error);
         });
-      }).catch(error => {
-        console.error('Error creating reception team:', error);
+      } else {
         Swal.fire({
           icon: "error",
           title: "Oops...",
-          text: "Ocorreu um erro ao cadastrar novo instrumento"
+          text: "Por favor, preencha todos os campos."
         });
-        reject(error);
+        reject(new Error('Invalid input'));
+      }
+    });
+  }
+
+  createMusician(params: Musician): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      if (params.name.length > 0 && params.instrument.length > 0 && params.restrictions.length > 0) {
+        this.firestore.collection('musician').add({
+          name: params.name,
+          instrument: params.instrument,
+          restrictions: params.restrictions,
+        }).then(musicianRef => {
+          const musicianId = musicianRef.id;
+          return this.firestore.doc(`musician/${musicianId}`).update({
+            id: musicianId
+          }).then(() => {
+            return this.firestore.doc(`instrument/${params.instrument}`).update({
+              qtdMusicians: firebase.firestore.FieldValue.increment(1)
+            });
+          }).then(() => {  
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Novo Músico adicionado",
+              showConfirmButton: true,
+              confirmButtonText: "Ok",
+              timer: 1500
+            });
+            resolve();
+          });
+        }).catch(error => {
+          console.error('Error creating visitor:', error);
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Ocorreu um erro ao cadastrar novo músico"
+          });
+          reject(error);
+        });
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Por favor, preencha todos os campos."
+        });
+        reject(new Error('Invalid input'));
+      }
+    });
+  }
+
+
+  async findInstruments(): Promise<Instrument[]> {
+    try {
+      const collectionRef = this.firestore.collection('instrument');
+      const querySnapshot = await collectionRef.get().toPromise();
+
+      if (!querySnapshot) {
+        console.error('Erro ao buscar documentos');
+        return [];
+      }
+
+      if (querySnapshot.empty) {
+        console.log('Nenhum documento encontrado');
+        return [];
+      }
+
+      const documents: Instrument[] = [];
+      querySnapshot.forEach(doc => {
+        documents.push(doc.data() as Instrument);
       });
-    } else {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Por favor, preencha todos os campos."
-      });
-      reject(new Error('Invalid input'));
+
+      return documents;
+    } catch (error) {
+      console.error('Erro ao buscar documentos:', error);
+      throw error;
     }
-  });
-}
+  }
+
 
 }
 
 type Instrument = {
   id: string;
   instrumentName: string;
+}
+
+type Musician = {
+  id: string;
+  name: string;
+  restrictions: String;
+  instrument: string;
 }
