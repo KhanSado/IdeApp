925a3e34d2db0e3dbee60178f07c521794581046
bersons <bersonintech@gmail.com>
2024-10-15 14:58:29 -0300

    gerando pdf da ata

--- src/app/pages/minutes/minute-uph-details/minute-uph-details.component.ts
+++ src/app/pages/minutes/minute-uph-details/minute-uph-details.component.ts
@@ -5,6 +5,7 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { Card } from 'src/app/models/Card';
 import { Minute } from 'src/app/models/Minute';
 import { UphService } from '../services/uph.service';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-minute-uph-details',
@@ -24,7 +25,6 @@ export class MinuteUphDetailsComponent implements OnInit {
 
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id') ?? '';
-    console.log('ID:', this.id);
     this.showDetails(this.id)
   }
 
@@ -33,42 +33,29 @@ export class MinuteUphDetailsComponent implements OnInit {
       const minute = await this.service.findMinuteById(id);
       this.minute = minute;
       console.log(this.minute);
-    //  this.cards = [
-    //     {
-    //       background: 'bg-c-blue',
-    //       title: 'Adultos',
-    //       icon: 'icon-users',
-    //       text: '',
-    //       number: (this.minute?.qtdAdultos ?? 0),
-    //       no: ''
-    //     },
-    //     {
-    //       background: 'bg-c-green',
-    //       title: 'Crianças e Adolescentes',
-    //       icon: 'icon-users',
-    //       text: '',
-    //       number: (this.cult?.qtdCriancasAdolescentes ?? 0),
-    //       no: ''
-    //     },
-    //     {
-    //       background: 'bg-c-yellow',
-    //       title: 'Visitas',
-    //       icon: 'icon-users',
-    //       text: '',
-    //       number: (this.cult?.qtdVisitas ?? 0),
-    //       no: ''
-    //     },
-    //     {
-    //       background: 'bg-c-red',
-    //       title: 'Publico Total',
-    //       icon: 'icon-users',
-    //       text: '',
-    //       number: (this.cult?.qtdAdultos ?? 0) + (this.cult?.qtdCriancasAdolescentes ?? 0) + (this.cult?.qtdVisitas ?? 0),
-    //       no: ''
-    //     }
-    //   ];
+    
     } catch (error) {
       console.error('Erro ao buscar documento:', error);
     }    
   }
+
+  generatePDF() {
+    const doc = new jsPDF();
+    const content = document.getElementById('pdfContent')?.innerText;
+  
+    if (content) {
+      const pageWidth = doc.internal.pageSize.getWidth(); 
+      const margin = 18; 
+      const maxLineWidth = pageWidth - margin * 2;
+  
+      const lines = doc.splitTextToSize(content, maxLineWidth);
+  
+      doc.text(lines, margin, 10);
+  
+      doc.save('Ata.pdf');
+    } else {
+      console.error('Elemento HTML não encontrado ou vazio');
+    }
+  }
+  
 }
