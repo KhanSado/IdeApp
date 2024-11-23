e8ffc30272c8c910e66bbb245be20aa0c1a61b23
bersons <bersonintech@gmail.com>
2024-11-23 15:32:32 -0300

    2.4.0 - change not found route

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { HomeService } from './service/home.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/not-authorized/not-authorized.component.ts
+++ src/app/pages/not-authorized/not-authorized.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/theme/shared/shared.module';
 
 @Component({
   selector: 'app-not-authorized',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SharedModule, RouterModule],
   templateUrl: './not-authorized.component.html',
   styleUrl: './not-authorized.component.scss'
 })
 export class NotAuthorizedComponent {
+  constructor(private router: Router) {}
 
+  goToHome() {
+    this.router.navigate(['/auth/signin']); // Substitua '/home' pela rota desejada
+  }
 }
