c1287479eb610f51958bcdc4c50430f91b8dfb65
bersons <bersonintech@gmail.com>
2024-11-23 16:27:10 -0300

    2.5.0 change home screen with cult infomration

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { HomeService } from './service/home.service';
 import { Component, OnInit } from '@angular/core';
+import { Cult } from 'src/app/models/Cult';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
 
+  cults: Cult[] = [];
+  currentYear: number = new Date().getFullYear();
+  cultCount: number = 0;
+  totalAdultos: number = 0;
+  totalCriancasAdolescentes: number = 0;
+  totalVisitas: number = 0;
+
   constructor(private homeService: HomeService) { }
 
   ngOnInit(): void {
+    this.loadCultCountForYear(this.currentYear);
 
+    this.homeService.getTotalPresencasNoAno(this.currentYear).subscribe(data => {
+      this.totalAdultos = data.totalAdultos;
+      this.totalCriancasAdolescentes = data.totalCriancasAdolescentes;
+      this.totalVisitas = data.totalVisitas;
+    });
   }
+
+  async loadCultCountForYear(year: number): Promise<void> {
+    try {
+      this.cultCount = await this.homeService.countCultsByYear(year);
+
+      if (this.cultCount === 0) {
+        console.log(`Nenhum culto encontrado para o ano ${year}`);
+      } else {
+        console.log(`Quantidade de cultos encontrados para o ano ${year}: ${this.cultCount}`);
+      }
+    } catch (error) {
+      console.error('Erro ao carregar a contagem de cultos:', error);
+    }
+  }
+  
 
 }
--- src/app/pages/home/service/home.service.ts
+++ src/app/pages/home/service/home.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { Cult } from 'src/app/models/Cult';
 
 @Injectable({
   providedIn: 'root'
@@ -11,30 +14,51 @@ export class HomeService implements OnInit{
   ngOnInit(): void {
   }
 
-  createData(){
-     this.firestore.collection('users').add({
-      name: 'John Doe',
-      email: 'johndoe@example.com'
-    });
-  }
-
-  findData(){
-    this.firestore.doc('users/123').get().subscribe((doc) => {
-      if (doc.exists) {
-        console.log('Documento encontrado:', doc.data());
-      } else {
-        console.log('Documento não encontrado');
+  async countCultsByYear(year: number): Promise<number> {
+    try {
+      // Filtra os cultos pelo ano
+      const collectionRef = this.firestore.collection('cult', ref =>
+        ref.where('data', '>=', `${year}-01-01`)
+           .where('data', '<', `${year + 1}-01-01`)
+           .orderBy('data')
+      );
+      const querySnapshot = await collectionRef.get().toPromise();
+      
+      if (!querySnapshot) {
+        console.error('Erro ao buscar documentos');
+        return 0;
       }
-    });
-  }
 
-  editData(){
-    this.firestore.doc('users/123').update({
-      email: 'janedoe@example.com'
-    });
+      return querySnapshot.size; // Retorna o número de cultos encontrados
+    } catch (error) {
+      console.error('Erro ao contar cultos:', error);
+      throw error;
+    }
   }
+  
+  getTotalPresencasNoAno(currentYear: number): Observable<{ totalAdultos: number, totalCriancasAdolescentes: number, totalVisitas: number }> {
+    const cultosRef = this.firestore.collection('cult', ref =>
+      ref.where('data', '>=', `${currentYear}-01-01`) // Início do ano
+         .where('data', '<', `${currentYear + 1}-01-01`) // Fim do ano
+    );
 
-  deleteData(){
-    this.firestore.doc('users/123').delete();
+    return cultosRef.snapshotChanges().pipe(
+      map(actions => {
+        let totalAdultos = 0;
+        let totalCriancasAdolescentes = 0;
+        let totalVisitas = 0;
+
+        actions.forEach(action => {
+          const cultoData = action.payload.doc.data() as Cult; 
+          
+          totalAdultos += cultoData.qtdAdultos || 0; 
+          totalCriancasAdolescentes += cultoData.qtdCriancasAdolescentes || 0; 
+          totalVisitas += cultoData.qtdVisitas || 0;  
+        });
+
+        return { totalAdultos, totalCriancasAdolescentes, totalVisitas };
+      })
+    );
   }
+  
 }
